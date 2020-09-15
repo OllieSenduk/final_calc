4f46124f38c41339bc2942b543135f169f3396a8
Oliver Senduk <oliver@ichistudios.com>
2020-09-15 10:37:03 +0200

    add helper

--- app/services/calculators/yearly_energy_price_calculator.rb
+++ app/services/calculators/yearly_energy_price_calculator.rb
@@ -1,29 +1,29 @@
 module Calculators
-class YearlyEnergyPriceCalculator 
-    def initialize(connection_year)
-        @kwh = connection_year.kwh
-        @year = connection_year.year
-        @total_remaining_kwh = @kwh
-        @total_price = 0
+    class YearlyEnergyPriceCalculator 
+        def initialize(connection_year)
+            @kwh = connection_year.kwh
+            @year = connection_year.year
+            @total_remaining_kwh = @kwh
+            @total_price = 0
 
-        init_calc
-    end
-
-    def init_calc
-        Year.KWH_TIERS.each_with_index do |kwh_tier, index|
-        if @total_remaining_kwh - kwh_tier >= 0 
-            calc_total_price(kwh_tier)
-            @total_remaining_kwh -= kwh_tier
-            raise
-        else
-            calc_total_price(@total_remaining_kwh)
-            break
+            init_calc
         end
-        return @total_price
-    end
 
-    def calc_total_price(multiplier)
-        @total_price += multiplier * year.send("tier_#{index+1}_price"
+        def init_calc
+            Year::KWH_TIERS.each_with_index do |kwh_tier, index|
+                if @total_remaining_kwh - kwh_tier >= 0 
+                    calc_total_price(kwh_tier, index)
+                    @total_remaining_kwh -= kwh_tier
+                else
+                    calc_total_price(@total_remaining_kwh, index)
+                    break
+                end
+            end
+            return @total_price
+        end
+
+        def calc_total_price(_multiplier, _index)
+            @total_price += _multiplier * @year.send("tier_#{_index+1}_price")
+        end
     end
-end
 end
